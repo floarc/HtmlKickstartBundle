8b9bb0f3289f0d6fc1ec6298a84c0c7d72b41a5b
Florent Arcambal <florent.arcambal@free.fr>
2012-02-15 12:57:46 +0100

    Ajout du projet git html-kickstart de 99lime as a submodule Ajout de la structure de base du Bundle

---
+++ NinetyNineLimeHtmlKickstartBundle.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * This file is not a part of the 99Lime HtmlKickstartBundle project.
+ *
+ * (c) Farc
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace NinetyNineLime\HtmlKickstartBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NinetyNineLimeHtmlKickstartBundle extends Bundle
+{
+
+}
